55de6d975bb676530372d9e4696e8646b245a500
Fields Marshall <fields.marshall@gmail.com>
2025-04-15 05:31:30 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import vercel from "@astrojs/vercel";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server", // enables SSR & API support
-  adapter: vercel(), // Vercel adapter for deployment
+  output: "static", // enables SSR & API support
+  adapter: vercelStatic(), // Vercel adapter for deployment
   integrations: [tailwind(), mdx()],
   build: {
     rollupOptions: {
