da7d6a7b2ec9ca81bf3b9d2a5129b6cc342f60ea
Fields Marshall <fields.marshall@gmail.com>
2025-04-15 05:04:12 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import vercel from "@astrojs/vercel/server";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
